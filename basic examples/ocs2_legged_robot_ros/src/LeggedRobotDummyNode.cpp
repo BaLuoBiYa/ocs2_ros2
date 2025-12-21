bcf0c1713f9c0a6fb55e89c90d466bab27f2aa6f
BaLuoBiYa <Louie.LiuHaoYu@outlook.com>
2025-12-21 13:15:15 +0800

    init for luo

--- basic examples/ocs2_legged_robot_ros/src/LeggedRobotDummyNode.cpp
+++ basic examples/ocs2_legged_robot_ros/src/LeggedRobotDummyNode.cpp
@@ -34,6 +34,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <ocs2_ros_interfaces/mrt/MRT_ROS_Interface.h>
 
 #include "ocs2_legged_robot_ros/visualization/LeggedRobotVisualizer.h"
+#include "ocs2_legged_robot_ros/gazebo/MRT_ROS_Gazebo_Loop.h"
 #include "rclcpp/rclcpp.hpp"
 
 using namespace ocs2;
